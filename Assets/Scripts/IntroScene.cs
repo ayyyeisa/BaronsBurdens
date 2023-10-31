aefe89400d0394d4e8116b4c4689f9348cbbcc43
Isa <80214167+ayyyeisa@users.noreply.github.com>
2023-10-31 14:36:44 -0500

    added intro scene
    still needs to be connected back to main menu

--- Assets/Scripts/IntroScene.cs
+++ Assets/Scripts/IntroScene.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class IntroScene : MonoBehaviour
 {
     // Behavior for Intro Scene to load main menu scene once intro scene is done
+    VideoPlayer video;
 
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        StartCoroutine("IntroEnd");
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator IntroEnd()
     {
-        
+        while (video.isPlaying)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void OnVideoEnd()
+    {
+        SceneManager.LoadScene(1);
     }
 }
