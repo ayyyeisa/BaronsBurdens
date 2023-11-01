8e7069b60c7048e5ab36db1fe6d2b0deb225c5be
Isa <80214167+ayyyeisa@users.noreply.github.com>
2023-11-01 04:21:06 -0500

    worked on duel

--- Assets/Scripts/PlayerDuel.cs
+++ Assets/Scripts/PlayerDuel.cs
@@ -1,8 +1,11 @@
-//PlayerDuel.cs
-//Author(s): Carl Crumer, Isa Luluquisin
-//Creation Date: October 24 2023
-//
-// The script the player functions of the game,input, and User Interface
+///<summary>
+///PlayerDuel.cs 
+///
+///Author(s): Carl Crumer, Isa Luluquisin
+///Creation Date: October 24, 2023
+///
+///Description: The script the player functions of the game,input, and User Interface
+/// </summary>
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +18,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDuel : MonoBehaviour
 {
+    #region variables
     //Text for instructions, the player's lives, and a time
     //use this 
     public TMP_Text instructionText;
@@ -30,9 +34,10 @@ public class PlayerDuel : MonoBehaviour
     [SerializeField] private GameObject loseScene;
     //private InputAction parry;
     //private InputAction block;
-    //private InputAction attack;
+    private InputAction attack;
     private InputAction restart;
     private InputAction quit;
+    private bool spaceIsPressed;
 
     //player gets 2 lives, the game lasts for 20 seconds
     private int lives = 2;
@@ -44,16 +49,20 @@ public class PlayerDuel : MonoBehaviour
 
     private KeyCode[] validInputs = { KeyCode.F, KeyCode.A, KeyCode.Space };
 
+    #endregion
+
     // Start is called before the first frame update
     void Start()
-    {  
+    {
         //Enables action map
         EnableInputs();
 
-        isRunning = true;  //starts the timer
+        spaceIsPressed = false;
+        isRunning = false;
 
         timer = 0f;
         lives = 2;
+
         UpdateScreen();
     }
     //Updates the user interface,updates the player's lives and time remaining
@@ -68,8 +77,15 @@ public class PlayerDuel : MonoBehaviour
         // when the timer reaches the game duration. It also updates the user interface.
         void Update()
         {
-            //use some of this 
-            if (isRunning)
+            if(!isRunning)
+            {
+                if(spaceIsPressed)
+                {
+                isRunning = true; //starts timer
+                }
+            }
+            
+            if (spaceIsPressed && isRunning)
             {
                 //timer
                 timer += Time.deltaTime;
@@ -94,8 +110,10 @@ public class PlayerDuel : MonoBehaviour
             }
 
         }
+        
+
         // changes the isRunning boolean to false and changes instruction
-        //text on UI to read win or lose, and goes back to main menu
+        //text on UI to read win or lose
         
         private void EndGame()
         {
@@ -114,7 +132,7 @@ public class PlayerDuel : MonoBehaviour
     {
         //parry = playerInput.currentActionMap.FindAction("Parry");
         //block = playerInput.currentActionMap.FindAction("Block");
-        //attack = playerInput.currentActionMap.FindAction("Attack");
+        attack = playerInput.currentActionMap.FindAction("Attack");
         restart = playerInput.currentActionMap.FindAction("Restart");
         quit = playerInput.currentActionMap.FindAction("Quit");
 
@@ -122,6 +140,19 @@ public class PlayerDuel : MonoBehaviour
         quit.started += Quit_started;
     }
 
+    #region Input Actions
+    private void Attack_started(InputAction.CallbackContext obj)
+    {
+        if(!spaceIsPressed)
+        {
+            startGameScreen.gameObject.SetActive(false);
+            spaceIsPressed = true;
+        }
+        else if(spaceIsPressed)
+        {
+
+        }
+    }
     private void Quit_started(InputAction.CallbackContext obj)
     {
         //Loads Main Menu scene
@@ -131,15 +162,18 @@ public class PlayerDuel : MonoBehaviour
     private void Restart_started(InputAction.CallbackContext obj)
     {
         //Reloads current scene
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
 
-        // CheckInput is called when the player presses a key during an active game.
-        // It validates the key pressed against the allowed inputs and provides feedback
-        // to the player. If the input is correct, the game continues, but if it's incorrect,
-        // the player loses a life, and the game may end if there are no more lives.
-        // It also updates the user interface to reflect the result.
-        private void CheckInput() {
+    #endregion
+
+    // CheckInput is called when the player presses a key during an active game.
+    // It validates the key pressed against the allowed inputs and provides feedback
+    // to the player. If the input is correct, the game continues, but if it's incorrect,
+    // the player loses a life, and the game may end if there are no more lives.
+    // It also updates the user interface to reflect the result.
+    private void CheckInput() 
+    {
             KeyCode keyPressed = KeyCode.None;
 
             if (Input.GetKeyDown(KeyCode.F))
@@ -199,8 +233,11 @@ public class PlayerDuel : MonoBehaviour
             Debug.Log("Entered Display Random");
 
         }
-        //GetRandomInstruction holds a list of instructions and randomly choses 
-        //an instruction to produce for the user
+        ///<summary>
+        ///Description: GetRandomInstruction holds a list of instructions and randomly choses an instruction to produce for the user
+        ///<return>Returns key instructions the player must press 
+        ///</return>
+        ///</summary>
         private string GetRandomInstruction()
         {
             string[] instructions = { "Parry (Press F)", "Block (Press A)", "Attack (Press Space)" };
