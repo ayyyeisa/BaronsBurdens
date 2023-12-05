07874db4e8db93eaa3c01bedafe792a78ffaf505
Isa <80214167+ayyyeisa@users.noreply.github.com>
2023-12-04 22:34:39 -0600

    added comments

--- Assets/Scripts/SceneAndMenu/VidPlayer.cs
+++ Assets/Scripts/SceneAndMenu/VidPlayer.cs
@@ -1,3 +1,11 @@
+/*****************************************************************************
+// File Name : VidPlayer.cs
+// Author : Isa Luluquisin
+// Creation Date : November 28, 2023
+//
+// Brief Description : This is a file that handles the loading of main menu
+                        after the intro scene-- specifically used for the webbuild.
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +14,7 @@ using UnityEngine.Video;
 
 public class VidPlayer : MonoBehaviour
 {
+    [Tooltip("Filename of the video that must be played")]
     [SerializeField] string videoFileName;
 
     void Start()
@@ -17,6 +26,7 @@ public class VidPlayer : MonoBehaviour
     {
         VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
 
+        //plays the video if there is a videoplayer present
         if(videoPlayer)
         {
             string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
